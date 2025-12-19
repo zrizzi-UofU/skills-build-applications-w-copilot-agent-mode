b5230850721df6191471ff778eaa94d4d7c36192
zrizzi-UofU <zach.rizzi@hsc.utah.edu>
2025-12-19 22:26:36 +0000

    feat: set up routing and import components in App.js

--- octofit-tracker/frontend/src/App.js
+++ octofit-tracker/frontend/src/App.js
@@ -1,4 +1,10 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Activities from './components/Activities';
+import Leaderboard from './components/Leaderboard';
+import Teams from './components/Teams';
+import Users from './components/Users';
+import Workouts from './components/Workouts';
 
 function App() {
   return (
--- octofit-tracker/frontend/src/index.js
+++ octofit-tracker/frontend/src/index.js
@@ -1,4 +1,10 @@
 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
